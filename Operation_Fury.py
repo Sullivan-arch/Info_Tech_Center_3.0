364922f68eb578fd23273b81140d745e7c67a69b
Sullivan A <sullivanabegg8@gmail.com>
2022-10-03 08:39:10 -0400

    Import Time Library - Call Sleep Function

--- Operation_Fury.py
+++ Operation_Fury.py
@@ -6,7 +6,12 @@
 Our Welcome Screen will start our program letting
 drivers know that the InfoTechCenter OS is loading. 
 """
+#Import Libraries here
+from time import sleep#We imported the Sleep function from the Time library
 
 print("\n\nWelcome to Operation Fury Info_Tech Center")
 
+sleep(2)
+
 print("\nOperation Fury's Operation System is Booting up")
+
