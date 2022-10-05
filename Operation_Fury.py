acc9609fc7d6f49b3882b9e5c9f93cc58f0c8190
Sullivan A <sullivanabegg8@gmail.com>
2022-10-05 08:39:10 -0400

    Initial Weather Commit

---
+++ Operation_Fury.py
@@ -0,0 +1,11 @@
+#Weather
+#Developer:Sullivan Abegg
+#Version 1.0
+
+"""
+Create a function for our current weather using the
+random.choice function to determine what the weather
+is picking from a list - using If, elif & Else statements
+to check the condition and print a specific corresponding
+print line
+"""
