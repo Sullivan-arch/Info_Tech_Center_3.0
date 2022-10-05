99803bf81c95163f98aeccedcf9b08bea166491d
Sullivan A <sullivanabegg8@gmail.com>
2022-10-05 08:06:26 -0400

    Add for Loop "OS Booting Up"

--- Operation_Fury.py
+++ Operation_Fury.py
@@ -13,9 +13,9 @@ print("\033[1;34;40m  \n")#033 is the escape code, 1=Style, 32=Text color, 40m=b
 #Black-30, Red-31, Green-32, Yellow-33, Blue-34, Purple-35, Cyan-36, White-37
 
 print("\n\nWelcome to Operation Fury Info_Tech Center")
-
 sleep(2)
-
 print("\033[1;33;40m \nOperation Fury's Operation System is Booting up")
 
-print("")
+for i in range(2):
+    print("OS Booting Up")
+    sleep(1)
