6959d7a91a2431fc6bfc1626f8ce7c8cb3625f9a
Sullivan A <sullivanabegg8@gmail.com>
2022-10-13 07:57:50 -0400

    Added more elif and else statements, increased smoothness - Stable

--- Operation_Fury.py
+++ Operation_Fury.py
@@ -29,11 +29,13 @@ def list_of_gas_stations():
 def gas_level_alert():
     miles_to_gas_station_low = round(random.uniform(1, 25), 1)
     miles_to_gas_station_quarter_tank = round(random.uniform(26, 50), 1)
+    miles_to_gas_station_half = round(random.uniform(51, 100), 1)
     if gas_level_indicator == "Empty":
         print("***WARNING YOU ARE ON EMPTY***")
         sleep(1)
         print("Calling Emergency Contact")
     elif gas_level_indicator == "Low":
+        sleep(1)
         print("**WARNING**")
         sleep(1)
         print("Your gas tank is extremely low, checking Google Maps for the closest gas station")
@@ -46,6 +48,7 @@ def gas_level_alert():
         sleep(1)
         print("The closest gas station is", list_of_gas_stations(),"located",miles_to_gas_station_low,"miles away.")
     elif gas_level_indicator == "Quarter Tank":
+        sleep(1)
         print("**WARNING**")
         sleep(1)
         print("Your gas tank is 1/4 full, checking Google Maps for the closest gas station")
@@ -58,6 +61,15 @@ def gas_level_alert():
         sleep(1)
         print("The closest gas station is", list_of_gas_stations(),"located",miles_to_gas_station_quarter_tank,"miles away.")
     elif gas_level_indicator == "Half Tank":
+        sleep(2)
+        print("Your gas tank is half full, you have enough gas to get where your going")
+    elif gas_level_indicator == "Three Quarters Tank":
+        sleep(2)
+        print("Your gas tank is almost full, you have enough gas to get where your going")
+    else:
+        sleep(2)
+        print("Your gas tank is full, you can drive anywhere with gas to spare")
+
 
 
 gas_level_alert()
