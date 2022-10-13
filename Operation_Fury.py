27bf0f62a6903a45d7091f147c42aae0561f8c34
Sullivan A <sullivanabegg8@gmail.com>
2022-10-13 08:27:38 -0400

    Updated program for better readability

--- Operation_Fury.py
+++ Operation_Fury.py
@@ -28,7 +28,8 @@ for i in range(4):
     sleep(1)
 
 
-#Gas level function
+#Gas level function that tells users how much gas is left and the nearest gas
+#station by using a random library
 def gas_level_gauge():
     gas_level_list = ["Empty","Low","Quarter Tank","Half Tank","Three Quarters Tank","Full Tank"]
     current_gas_level = random.choice(gas_level_list)
@@ -45,7 +46,6 @@ def list_of_gas_stations():
 def gas_level_alert():
     miles_to_gas_station_low = round(random.uniform(1, 25), 1)
     miles_to_gas_station_quarter_tank = round(random.uniform(26, 50), 1)
-    miles_to_gas_station_half = round(random.uniform(51, 100), 1)
     if gas_level_indicator == "Empty":
         print("***WARNING YOU ARE ON EMPTY***")
         sleep(1)
@@ -54,7 +54,7 @@ def gas_level_alert():
         sleep(1)
         print("**WARNING**")
         sleep(1)
-        print("Your gas tank is extremely low, checking Google Maps for the closest gas station")
+        print("Your gas tank is extremely low, checking Google Maps for the closest gas station.")
         sleep(1)
         print("Searching.")
         sleep(1)
@@ -67,7 +67,7 @@ def gas_level_alert():
         sleep(1)
         print("**WARNING**")
         sleep(1)
-        print("Your gas tank is 1/4 full, checking Google Maps for the closest gas station")
+        print("Your gas tank is 1/4 full, checking Google Maps for the closest gas station.")
         sleep(1)
         print("Searching.")
         sleep(1)
@@ -78,17 +78,17 @@ def gas_level_alert():
         print("The closest gas station is", list_of_gas_stations(),"located",miles_to_gas_station_quarter_tank,"miles away.")
     elif gas_level_indicator == "Half Tank":
         sleep(2)
-        print("Your gas tank is half full, you have enough gas to get where your going")
+        print("Your gas tank is half full, you have enough gas to get where your going.")
     elif gas_level_indicator == "Three Quarters Tank":
         sleep(2)
-        print("Your gas tank is almost full, you have enough gas to get where your going")
+        print("Your gas tank is almost full, you have enough gas to get where your going.")
     else:
         sleep(2)
-        print("Your gas tank is full, you can drive anywhere with gas to spare")
+        print("Your gas tank is full, you can drive anywhere with gas to spare.")
 
 
 
-gas_level_alert()
+
 
 
 #Weather
@@ -117,36 +117,45 @@ print("Checking weather condition..")
 sleep(1)
 print("Checking weather condition...")
 sleep(1)
+
+
 def vehicle_response_system():
     if weatherAlert == "Icy":
         print("\nVRS has changed your alarm 30 minutes earlier based on the NWS forcast of",weatherAlert)
         sleep(1)
-        print("VRS will only allow your car to go 30 MPH")
+        print("VRS will only allow your car to go 30 MPH.")
     elif weatherAlert == "Snow":
         print("\nVRS has changed your alarm 15 minutes earlier based on the NWS forcast of", weatherAlert)
         sleep(1)
-        print("VRS will only allow your car to go 50 MPH")
+        print("VRS will only allow your car to go 50 MPH.")
     elif weatherAlert == "Rain":
         print("\nVRS has changed your windshield wipers to on based on the NWS forcast of", weatherAlert)
         sleep(1)
-        print("VRS will only allow your car to go 65 MPH")
+        print("VRS will only allow your car to go 65 MPH.")
     elif weatherAlert == "Windy":
         print("\nVRS has changed your alarm 5 minutes earlier based on the NWS forcast of", weatherAlert)
         sleep(1)
-        print("VRS will only allow your car to go 70 MPH")
+        print("VRS will only allow your car to go 70 MPH.")
     elif weatherAlert == "Foggy":
         print("\nVRS has changed your alarm 20 minutes earlier; Your headlights have been turned on based on the NWS forcast of", weatherAlert)
         sleep(1)
-        print("VRS will only allow your car to go 40 MPH")
+        print("VRS will only allow your car to go 40 MPH.")
     elif weatherAlert == "Storming":
         print("\nVRS has changed your alarm 15 minutes earlier; Your windshield wipers have been turned on based on the NWS forcast of", weatherAlert , )
         sleep(1)
-        print("VRS will only allow your car to go 50 MPH")
+        print("VRS will only allow your car to go 50 MPH.")
     else:
         print("\nThe weather today is", weatherAlert,"Have a great day")
         sleep(1)
-        print("VRS will allow your car to go 90 MPH")
+        print("VRS will allow your car to go 90 MPH.")
 
+
+
+#******************************
+
+#Call Functions Here...
 vehicle_response_system()
+gas_level_alert()
+
 
 
